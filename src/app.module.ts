2db39a7d7aeae76fd8c2506a76fe9d61fde75993
Sérgio Nogueira <sergio.nogueira@ctw.bmwgroup.com>
2023-07-11 23:19:20 +0100

    CHANGE
    A

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,9 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm/dist/typeorm.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TodosModule } from './todos/todos.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot(),
-    TodosModule
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,8 @@
-import {INestApplication, ValidationPipe} from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap(): Promise<void> {
-  const app: INestApplication = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-  app.enableCors();
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
