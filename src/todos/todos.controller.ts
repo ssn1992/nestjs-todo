d7775d69639cc6ed467d053dc41c044172d8223f
Sérgio Nogueira <sergio.nogueira@ctw.bmwgroup.com>
2023-07-11 22:43:16 +0100

    Nestjs app

---
+++ src/todos/todos.controller.ts
@@ -0,0 +1,41 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { ParseIntPipe } from '@nestjs/common/pipes/parse-int.pipe';
+import { Todo } from './todo.entity';
+import { TodosService } from './todos.service';
+
+@Controller('todos')
+export class TodosController {
+  constructor(private todosService: TodosService) {}
+
+  @Get()
+  findAll(): Promise<Todo[]> {
+    return this.todosService.getTodos();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id): Promise<Todo> {
+    return this.todosService.findOne(id);
+  }
+
+  @Post() create(@Body() todo: Todo): Promise<Todo> {
+    return this.todosService.createTodo(todo);
+  }
+
+  @Patch(':id')
+  async editTodo(@Body() todo: Todo, @Param('id') id: number): Promise<Todo> {
+    return await this.todosService.editTodo(id, todo);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id): Promise<string> {
+    return this.todosService.remove(id);
+  }
+}
