59c981323bda85db1ed6335cc718d2b2a1eb5a88
Sérgio Nogueira <sergio.nogueira@ctw.bmwgroup.com>
2023-07-12 08:47:55 +0100

    Test
    est

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return process.env.RDS_HOSTNAME + ' ' + process.env.RDS_PORT + ' ' + process.env.RDS_USERNAME + ' ' + process.env.RDS_PASSWORD + ' ' + process.env.RDS_DB_NAME
   }
 }
