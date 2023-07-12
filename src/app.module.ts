d3c67e638b0d6d6b70521316677b744b9f7039b0
Sérgio Nogueira <sergio.nogueira@ctw.bmwgroup.com>
2023-07-12 14:20:44 +0100

    Change conneciton
    Change conneciton

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,16 @@ import { TodosModule } from './todos/todos.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.RDS_HOSTNAME,
+      port: Number(process.env.RDS_PORT),
+      username: process.env.RDS_USERNAME,
+      password: process.env.RDS_PASSWORD,
+      database: process.env.RDS_DB_NAME,
+      entities: ["dist/**/*.entity{.ts,.js}"],
+      synchronize: true,
+    }),
     TodosModule
   ],
   controllers: [AppController],
