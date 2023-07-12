c8e3087a28ce078c78564ceffc90f1fb5f7a0ba1
Sérgio Nogueira <sergio.nogueira@ctw.bmwgroup.com>
2023-07-12 09:06:04 +0100

    asd
    asd

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return process.env.PORT + ' \n' + process.env.RDS_HOSTNAME + ' \n' + process.env.RDS_PORT + ' \n' + process.env.RDS_USERNAME + ' \n' + process.env.RDS_PASSWORD + ' \n' + process.env.RDS_DB_NAME
+    return 'Hello World!';
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
+import {INestApplication, ValidationPipe} from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+async function bootstrap(): Promise<void> {
+  const app: INestApplication = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
