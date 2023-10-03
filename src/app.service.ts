6bda0f84577205d885a51f0d5b390efdab1c8e96
Sérgio Nogueira <ssn_1992@hotmail.com>
2023-10-03 20:15:28 +0100

    Change hello world
    Change hello world

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World Atec!';
+    return 'Hello World Atec 06PL!';
   }
 }
